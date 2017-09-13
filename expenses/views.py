ff792c31ff3752fdb343e4a541fcb99d5571a764
Philippe Guyard <pguyard@gmail.com>
2017-09-13 18:35:16 +0100

    intermediate commit

--- expenses/views.py
+++ expenses/views.py
@@ -4,9 +4,7 @@ from __future__ import unicode_literals
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.http import JsonResponse
-from django.urls import reverse_lazy
+#from django.http import JsonResponse
 
 from expenses.models import Expenses
 
@@ -18,7 +16,7 @@ class ExpenseCreate(LoginRequiredMixin, CreateView):
     model = Expenses
     fields = ['category', 'date', 'amount', 'notes']
     template_name = 'expenses_add.html'
-    success_url = "/expenses"  
+    success_url = "/expenses"
     # def form_invalid(self, form):
     #     return JsonResponse(form.errors, status=400)
     def form_valid(self, form):
@@ -29,11 +27,10 @@ class ExpenseUpdate(LoginRequiredMixin, UpdateView):
     model = Expenses
     fields = ['category', 'date', 'amount', 'notes']
     template_name = 'expenses_add.html'
-    success_url = "/expenses"  
+    success_url = "/expenses"
     def form_valid(self, form):
         return super(ExpenseUpdate, self).form_valid(form)
 
 class ExpenseDelete(LoginRequiredMixin, DeleteView):
     model = Expenses
     success_url = "/expenses/"
-       
