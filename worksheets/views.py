15a062bae094cf36e67b8ee09a300d5a2ff9dfd4
Philippe Guyard <pguyard@gmail.com>
2017-09-13 20:31:09 +0100

    intermediate commit

--- worksheets/views.py
+++ worksheets/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from models import Jobs
+from django.views.generic import ListView
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class WSView(LoginRequiredMixin, TemplateView):
+class WSView(LoginRequiredMixin, ListView):
     template_name = "WS.html"
+    model = Jobs
