98b873cf000f5ade0af6e5515d791aa886fc1300
Philippe Guyard <pguyard@gmail.com>
2017-09-25 14:53:37 +0100

    fixed worksheet

--- worksheets/urls.py
+++ worksheets/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'(?P<pk>[0-9]+)/delete/$', JobDelete.as_view(), name='job_delete'),
     url(r'(?P<pk>[0-9]+)/check_in/$', JobCheckIn.as_view(), name='job_check_in'),
     url(r'invoices/$', Invoice.as_view(), name='invoices'),
-    url(r'(?P<pk>[0-9]+)/details/$', JobDetails.as_view(), name='job_check_in'),
+    url(r'(?P<pk>[0-9]+)/details/$', JobDetails.as_view(), name='job_details'),
     url(r'payment/$', Payment.as_view(), name='payment'),
 ]
