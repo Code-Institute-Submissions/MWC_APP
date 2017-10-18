478c9198dc6b098fe9ea5b0c5f9aa3a10272c07b
Philippe Guyard <pguyard@gmail.com>
2017-10-18 10:36:06 +0100

    fixed franchise on admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
+from franchises.models import Franchise
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-from franchises .models import Franchise
+from franchises.models import Franchise
 
 
 class User(AbstractUser):
