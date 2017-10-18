77121039e8280e79ae422168cd6f5fed85627d5e
Philippe Guyard <pguyard@gmail.com>
2017-10-18 11:02:22 +0100

    fixed user manager issue

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
