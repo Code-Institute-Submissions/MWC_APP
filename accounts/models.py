1178cc4a917b6079b154e427de9d4c3b75f7815e
Philippe Guyard <pguyard@gmail.com>
2017-10-18 09:55:50 +0100

    make it better

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-from franchises .models import Franchise
+from franchises.models import Franchise
 
 
 class User(AbstractUser):
     franchise = models.ForeignKey(
-        Franchise, null=True, blank=True, on_delete=models.PROTECT)
+        Franchise, null=False, blank=False, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.get_full_name()
